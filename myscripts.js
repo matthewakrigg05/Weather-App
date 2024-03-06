7f308ca9a783e264576b25c97b9e1af2c6205c6e
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-03-06 18:38:15 +0000

    Displays weather and added reminder file (#4)

--- myscripts.js
+++ myscripts.js
@@ -4,21 +4,16 @@ let nameVal = document.querySelector('.name');
 let temp = document.querySelector('.temp');
 let desc = document.querySelector('.desc');
 
-
-
 button.addEventListener('click', function(){
-    fetch("https://api.weatherbit.io/v2.0/current?city=${inputValue.value}&key=32165b4ed93b479c8ab5b330474d6756&include=minutely")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=8dee5f5a933b8f9547bfb2b995ef2d91`)
     .then(response => response.json())
-    console.log(response)
-    .then(displayData)
+    .then(
+        displayData)
     .catch(err => alert("Invalid City Name"))
 
 })
 
 const displayData=(weather)=>{
-    temp.innerText='${weather.main.temp}C'
-    desc.innerText='${weather.weather[0].main}'
+    temp.innerText=`${weather.main.temp}C`
+    desc.innerText=`${weather.weather[0].main}`
 }
-
-
-
