d0419b4e6ce2ee76604d513915b1edd4d0e347ac
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-03-07 21:35:02 +0000

    added font from google and allow enter key to enter input (#9)

--- myscripts.js
+++ myscripts.js
@@ -4,14 +4,24 @@ let nameVal = document.querySelector('.name');
 let temp = document.querySelector('.temp');
 let desc = document.querySelector('.desc');
 
+var enterKey = document.getElementById("searchBox")
+
 button.addEventListener('click', function(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=8dee5f5a933b8f9547bfb2b995ef2d91`)
     .then(response => response.json())
     .then(
         displayData)
     .catch(err => alert("Invalid City Name"))
-
 })
+
+enterKey.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+
+    document.getElementById("button").click();
+    }
+});
+
 
 const displayData=(weather)=>{
     temp.innerText=`${weather.main.temp}C`
