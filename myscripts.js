2ee7dd88b48f669da4b044104267a56584d1d534
Matthew Akrigg <138000221+matthewakrigg05@users.noreply.github.com>
2024-08-07 17:16:41 +0100

    Style changes (#24)
    * feat: working backgrounds and transitions
    
    * refactor: tiny cleanup
    
    * feat: new weather images
    
    * feat: styling changes to searchbox
    
    * feat: changes to style of box containing information
    
    * feat: restyled and planning to use bootstrap for responsiveness
    
    * feat: capitalised desc
    
    * feat: edited capitalised desc

--- myscripts.js
+++ myscripts.js
@@ -10,7 +10,7 @@ let locationIcon = document.querySelector('.weather-icon')
 var enterKey = document.getElementById("searchBox")
 
 button.addEventListener('click', function(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=8dee5f5a933b8f9547bfb2b995ef2d91`)
     .then(response => response.json())
     .then(
         displayData)
@@ -30,10 +30,11 @@ const displayData=(weather)=>{
     feelsLike.innerText = `Feels like ${Math.round(weather.main.feels_like)}°C`
     max.innerText = `Maximum temperature: ${Math.round(weather.main.temp_max)}°C`
     min.innerText = `Minimum temperature: ${Math.round(weather.main.temp_min)}°C`
-    desc.innerText=`${weather.weather[0].description}`
+    desc.innerText=`${weather.weather[0].description}`.charAt(0).toUpperCase() + `${weather.weather[0].description}`.slice(1);
 
     var icon = weather.weather[0].icon;
     locationIcon.innerHTML = `<img src="icons/${icon}.png" alt=${icon}>`;
     document.body.setAttribute("style", "background-image: url('weather-images/" + icon + ".jpg'); transition: linear 1.25s")
 }
 
+// next idea is to clear search each when start typing again
